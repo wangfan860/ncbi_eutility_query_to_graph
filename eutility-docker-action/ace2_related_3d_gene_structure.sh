4e8abf01f8abaa94e06d21950627dcbd76c7d16b
wangfan860 <fan1@uchicago.edu>
2021-08-09 16:44:24 -0500

    My 2nd action is ready

--- eutility-docker-action/ace2_related_3d_gene_structure.sh
+++
@@ -1,7 +0,0 @@
-esearch -db gene -query "ace2[gene] AND human[orgn]" | \
-elink -target structure -name gene_structure | \
-esummary -mode json > newest_structure.json
-
-# to list all the available links from pccompound
-# https://eutils.ncbi.nlm.nih.gov/entrez/eutils/einfo.fcgi?db=pccompound&retmode=json
-# detailed documentation: https://www.ncbi.nlm.nih.gov/books/NBK179288/
